37cbe8adaa4b49b8e6471f4fc1da1e7cbf04b812
Jagadees71098 <jagadeeswaran71098@gmail.com>
2024-08-05 08:54:19 +0530

    Update ProfitCard.js

--- src/components/ProfitCard.js
+++ src/components/ProfitCard.js
@@ -3,7 +3,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
-import { Box, Stack } from '@mui/material';
+import { Grid, Box, Stack } from '@mui/material';
 import CircleChart from './CircleChart';
 
 export default function ProfitCard() {
@@ -11,22 +11,26 @@ export default function ProfitCard() {
         <>
             <Card className='styledCard'>
                 <CardContent>
-                    <Stack direction={'row'} alignItems={'center'} justifyContent={'space-between'}>
-                        <Box>
-                            <Typography variant="body1" mt={1} fontWeight={300}>
-                                Net Profit
-                            </Typography>
-                            <Typography variant="h4" fontWeight={500} mt={3} gutterBottom>
-                                $6759.25
-                            </Typography>
-                            <Typography variant="h6" color={'success.main'}>
-                                <Stack direction={'row'} alignItems={'center'}><ArrowDropUpIcon /> <Typography variant="span">3%</Typography></Stack>
-                            </Typography>
-                        </Box>
-                        <Box>
-                            <CircleChart/>
-                        </Box>
-                    </Stack>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} md={12} lg={6}>
+                            <Box>
+                                <Typography variant="body1" mt={1} fontWeight={300}>
+                                    Net Profit
+                                </Typography>
+                                <Typography variant="h4" fontWeight={500} mt={3} gutterBottom>
+                                    $6759.25
+                                </Typography>
+                                <Typography variant="h6" color={'success.main'}>
+                                    <Stack direction={'row'} alignItems={'center'}><ArrowDropUpIcon /> <Typography variant="span">3%</Typography></Stack>
+                                </Typography>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12} md={12} lg={6}>
+                            <Box>
+                                <CircleChart />
+                            </Box>
+                        </Grid>
+                    </Grid>
                 </CardContent>
             </Card>
         </>
